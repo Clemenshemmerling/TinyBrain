a22adc1ed89aab1e92b5a23302432962435cce5b
Clemens Hemmerling <clemens.hemmerling@mangopractice.com>
2025-04-10 19:46:15 -0600

    Initial Commit

---
+++ TinyBrain Watch App/TinyBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyBrainApp.swift
+//  TinyBrain Watch App
+//
+//  Created by Clemens Hemmerling on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TinyBrain_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinyBrain Watch AppTests/TinyBrain_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinyBrain_Watch_AppTests.swift
+//  TinyBrain Watch AppTests
+//
+//  Created by Clemens Hemmerling on 10/04/25.
+//
+
+import Testing
+@testable import TinyBrain_Watch_App
+
+struct TinyBrain_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
